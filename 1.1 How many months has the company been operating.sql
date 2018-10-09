b84444d6154a3b510d4cbfdff2bdf6741492a3c4
Fred-Triest <42990124+Fred-Triest@users.noreply.github.com>
2018-10-08 21:56:27 -0700

    Delete 1.1 How many months has the company been operating.sql

--- 1.1 How many months has the company been operating.sql
+++
@@ -1,7 +0,0 @@
---1.1 Use MIN and MAX aggregate functions to retrieve
---the lowest and highest date values from subscription_start
---column from the subscriptions table
-SELECT
-  MIN(subscription_start) AS first_subscription_date,
-  MAX(subscription_start) AS last_subscription_date
-FROM subscriptions;
