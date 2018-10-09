6eb5d0d4b0ada2cea4425538e8c241313fa444f5
Fred-Triest <42990124+Fred-Triest@users.noreply.github.com>
2018-10-08 21:50:33 -0700

    Add files via upload

---
+++ 1.0 Get familiar with the company.sql
@@ -0,0 +1,10 @@
+--1.0 SELECT all columns from subscriptions table
+--and LIMIT query to 100 rows
+SELECT *
+FROM subscriptions
+LIMIT 100;
+--SELECT and GROUP BY the segment column for concrete
+--confirmation on all segments
+SELECT segment
+FROM subscriptions
+GROUP BY 1;
