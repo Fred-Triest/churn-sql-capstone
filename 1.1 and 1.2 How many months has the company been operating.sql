365ab94fdd471478eb24c77e8d5049beb6db6002
Fred-Triest <42990124+Fred-Triest@users.noreply.github.com>
2018-10-11 21:36:36 -0700

    Update 1.1 and 1.2 How many months has the company been operating.sql

--- 1.1 and 1.2 How many months has the company been operating.sql
+++ 1.1 and 1.2 How many months has the company been operating.sql
@@ -1,7 +1,7 @@
 --1.1 and 1.2 Use MIN and MAX aggregate functions to retrieve
---the lowest and highest date values from subscription_start
+--the first and latest date values from subscription_start
 --column from the subscriptions table
 SELECT
-  MIN(subscription_start) AS first_subscription_date,
-  MAX(subscription_start) AS last_subscription_date
+  MIN(subscription_start) AS first_subscription_start_date,
+  MAX(subscription_start) AS latest_subscription_start_date
 FROM subscriptions;
