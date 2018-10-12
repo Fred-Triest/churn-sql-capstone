38a00c007edf5b14de84bfdf904f07d80b9deec2
Fred-Triest <42990124+Fred-Triest@users.noreply.github.com>
2018-10-11 21:55:49 -0700

    Update 1.3 What segments of users exist.sql

--- 1.3 What segments of users exist.sql
+++ 1.3 What segments of users exist.sql
@@ -1,12 +1,11 @@
 --1.3 SELECT and GROUP BY the segment column for concrete
 --confirmation on all segments 
 SELECT segment
-FROM subscriptions
-GROUP BY 1;
+  FROM subscriptions
+ GROUP BY 1;
 --SELECT the DISTINCT COUNT of subscribers by id
 --and GROUP BY segment to get user count by segment
-SELECT DISTINCT
-  COUNT(id) AS number_of_subscribers,
-  segment
-FROM subscriptions
-GROUP BY 2;
+SELECT DISTINCT COUNT(id) AS number_of_subscribers,
+       segment
+  FROM subscriptions
+ GROUP BY 2;
